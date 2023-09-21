c832afc0ee302eaefd162139c60fb02ed989fc43
Tomer Berman <tomerber1@gmail.com>
2023-09-21 13:43:58 +0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,74 +1,17 @@
-variable "name" {
-  description = "The Prefix used for all resources in this example"
+variable "resource_group_location" {
+  type        = string
+  default     = "eastus"
+  description = "Location of the resource group."
 }
 
-variable "vm_password" {
-  description = "SSH password for the vm"
+variable "resource_group_name_prefix" {
+  type        = string
+  default     = "tb-resourcegroup"
+  description = "Prefix of the resource group name that's combined with a random ID so name is unique in your Azure subscription."
 }
 
-# VM Variables
-
-variable "azure_tenant_id" {
-  description = "Tenant ID"
-}
-
-variable "azure_subscription_id" {
-  description = "Subscription ID"
-}
-
-variable "azure_client_id" {
-  description = "Client ID"
-}
-
-variable "azure_client_secret" {
-  description = "Client Secret"
-}
-
-variable "vm_type" {
-  description = "VM Type"
-}
-
-variable "azure_location" {
-  description = "Location"
-}
-
-// Confluent Cloud variables
-variable "ccloud_bootstrap_servers" {
-  description = "Confluent Cloud username"
-}
-
-variable "ccloud_api_key" {
-  description = "Confluent Cloud password"
-}
-
-variable "ccloud_api_secret" {
-  description = "Confluent Cloud Provider"
-}
-
-variable "ccloud_topics" {
-  description = "Confluent Cloud topics to precreate"
-}
-
-variable "ccloud_sr_endpoint" {
-  description = "Confluent Cloud Schema Registry Endpoint"
-}
-
-variable "ccloud_sr_api_key" {
-  description = "Confluent Cloud Schema Registry API key"
-}
-
-variable "ccloud_sr_api_secret" {
-  description = "Confluent Cloud Schema Registry API Secret"
-}
-
-variable "bootstrap_vm_template_path" { 
-  description = "File path of the bootstrap_vm.tpl"
-}
-
-variable "bootstrap_docker_template_path" { 
-  description = "File path of the bootstrap_docker.tpl"
-}
-
-variable "docker_folder_path" { 
-  description = "Path for the docker folder to upload in the VM"
+variable "username" {
+  type        = string
+  description = "The username for the local account that will be created on the new VM."
+  default     = "azureadmin"
 }
